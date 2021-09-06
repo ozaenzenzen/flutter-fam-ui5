ea9ecbcb9dad280548d5c68b3be65522c554fee6
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-06 23:56:46 +0700

    update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fam_ui5/mainpage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -14,10 +15,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: MainPage(),
-      debugShowCheckedModeBanner: false,
-      
+    return ScreenUtilInit(
+      builder: () => GetMaterialApp(
+        home: MainPage(),
+        debugShowCheckedModeBanner: false,
+        title: "FAM Flutter UI 5",
+        defaultTransition: Transition.cupertino,
+      ),
     );
   }
 }
