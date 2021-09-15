0f88ff7400eb4b4a0d2706cdb6e34db74d1df8eb
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-15 14:53:00 +0700

    update credit menu

--- lib/page/menu_page/creditmenu.dart
+++ lib/page/menu_page/creditmenu.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreditMenu extends StatefulWidget {
-  const CreditMenu({ Key? key }) : super(key: key);
+  const CreditMenu({Key? key}) : super(key: key);
 
   @override
   _CreditMenuState createState() => _CreditMenuState();
 }
 
 class _CreditMenuState extends State<CreditMenu> {
+  ScreenUtil screenUtil = ScreenUtil();
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Credit Menu"),
+        child: Text(
+          "Credit Menu",
+          style: GoogleFonts.poppins(
+            fontSize: screenUtil.setSp(16),
+            fontWeight: FontWeight.w700,
+            color: Colors.grey,
+          ),
+        ),
       ),
     );
   }
