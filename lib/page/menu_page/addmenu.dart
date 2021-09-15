8e8c59d844fa3cc3f5473c22ca8ee56ad50cdaa3
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-15 14:52:55 +0700

    update add menu

--- lib/page/menu_page/addmenu.dart
+++ lib/page/menu_page/addmenu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AddMenu extends StatefulWidget {
   @override
@@ -6,11 +8,20 @@ class AddMenu extends StatefulWidget {
 }
 
 class _AddMenuState extends State<AddMenu> {
+  ScreenUtil screenUtil = ScreenUtil();
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("ADD Menu"),
+        child: Text(
+          "ADD Menu",
+          style: GoogleFonts.poppins(
+            fontSize: screenUtil.setSp(16),
+            fontWeight: FontWeight.w700,
+            color: Colors.grey,
+          ),
+        ),
       ),
     );
   }
