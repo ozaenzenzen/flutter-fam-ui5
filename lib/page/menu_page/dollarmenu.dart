f80b7786d54fc394a5395b2bc77e805a710d041c
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-09-15 14:53:05 +0700

    update dollar menu

--- lib/page/menu_page/dollarmenu.dart
+++ lib/page/menu_page/dollarmenu.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DollarMenu extends StatefulWidget {
-  const DollarMenu({ Key? key }) : super(key: key);
+  const DollarMenu({Key? key}) : super(key: key);
 
   @override
   _DollarMenuState createState() => _DollarMenuState();
 }
 
 class _DollarMenuState extends State<DollarMenu> {
+  ScreenUtil screenUtil = ScreenUtil();
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Dollar  Menu"),
+        child: Text(
+          "Dollar  Menu",
+          style: GoogleFonts.poppins(
+            fontSize: screenUtil.setSp(16),
+            fontWeight: FontWeight.w700,
+            color: Colors.grey,
+          ),
+        ),
       ),
     );
   }
